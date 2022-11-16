be6992c673176c96809a167970fd988a372a1047
Masaki Sakata <masaki@ga01.fai.cds.tohoku.ac.jp>
2022-11-16 21:21:18 +0900

    [upload]

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM nvcr.io/nvidia/pytorch:21.07-py3
 
 COPY entrypoint.sh /code/entrypoint.sh
-WORKDIR /code/fairseq
+WORKDIR /code/Geo-Ent-in-BERT
 
 ENTRYPOINT ["bash", "/code/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,23 @@
 echo 'Installing fairseq...'
 pip install --editable .
 pip install sacrebleu[ja]
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install scikit-learn
+pip install seaborn
+pip install optuna
+pip install gensim
+pip install transformers
+pip install fugashi
+pip install tqdm
+pip install seqeval
+pip install mlflow
+pip install scipy
+pip install jupyterlab
+pip install umap-learn
+pip install bokeh
+pip install datasets
 echo 'done.'
 
 bash
